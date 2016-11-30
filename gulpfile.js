624065949b1fd1ea97b45476499ec38d441e5ffc
Евгений Кириченко < >
2016-11-30 11:48:14 +0200

    git

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp');
+var concatCss = require('gulp-concat-css');
+var cleanCSS = require('gulp-clean-css');
+var includer = require('gulp-htmlincluder');
+var htmlmin = require('gulp-htmlmin');
+var connect = require('gulp-connect');
+var livereload = require('gulp-livereload')
+var spritecreator = require('gulp.spritesmith');
+
+gulp.task('sprite', function () {
+  var spriteData = gulp.src('dev/img/icon/*.png').pipe(spritecreator({
+    imgName: 'sprite.png',
+    cssName: 'sprite.css',
+    algorithm: 'binary-tree'
+  }));
+  spriteData.img.pipe(gulp.dest('build/img/'));
+  spriteData.css.pipe(gulp.dest('build/css/'));
+});
+gulp.task('concat', function () {
+  return gulp.src('dev/css/**/*.css')
+    .pipe(concatCss("styles.css"))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('build/css/'))
+    .pipe(connect.reload());
+});
+gulp.task('html', function() {
+	gulp.src('dev/**/*.html')
+    	.pipe(includer())
+      .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('build/'))
+        .pipe(connect.reload());;
+});
+gulp.task('connect', function() {
+  connect.server({
+    root: 'build',
+    livereload: true
+  });
+});
+gulp.task('default', function(){
+  gulp.start('connect', 'html', 'concat');
+
+  gulp.watch(['dev/**/*.html'], function(event) {
+    gulp.start('html');
+  });
+  gulp.watch(['dev/css/**/*.css'], function(event) {
+    gulp.start('concat');
+  });
+});
